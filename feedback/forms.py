94a8d2b43abe92861ba4fbb411f7ec1c353784a0
sairsc <sair@ignishealth.com>
2023-08-07 00:25:51 +0500

    Added crispy forms layout

--- feedback/forms.py
+++ feedback/forms.py
@@ -1,5 +1,7 @@
 from django import forms
 from .models import Feedback
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout, Submit, Row, Column, Div, HTML
 
 
 class FeedbackForm(forms.ModelForm):
@@ -8,8 +10,30 @@ class FeedbackForm(forms.ModelForm):
         fields = ("name", "email", "feedback")
         required_fields = ["name", "email", "feedback"]
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.layout = Layout(
+            Row(
+                Column("name", css_class="col-md-6 mb-0"),
+                Column("email", css_class="col-md-6 mb-0"),
+                css_class="row col-md-12",
+            ),
+            Row(
+                Column("feedback", css_class="form-group col-md-12 mb-2"),
+                css_class="row",
+            ),
+            Div(
+                HTML(
+                    '<button type="submit" class="save btn btn-secondary">Save</button>'
+                )
+            ),
+        )
+
     def clean_email(self):
         email = self.cleaned_data.get("email", "")
         if email.endswith("@softcatalyst.com"):
             return email
-        raise forms.ValidationError("Invalid Domain")
+        raise forms.ValidationError(
+            "Email is invalid. The email should be a softcatalyst email"
+        )
